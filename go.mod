86eb191a14f883df8e37455412e364f1e6a34fea
Laura <laura@wiese2.org>
2025-12-03 04:47:49 +0100

    wip

--- go.mod
+++ go.mod
@@ -4,12 +4,11 @@ go 1.25.4
 
 require (
 	github.com/alecthomas/kong v1.13.0
-	github.com/coalaura/plain v1.1.1
+	github.com/coalaura/plain v1.1.3
 )
 
 require (
-	github.com/containerd/console v1.0.5 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
-	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/term v0.34.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.37.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,18 +10,42 @@ import (
 
 var log = plain.New()
 
-var CLI struct {
+type Arguments struct {
 	Name        string `arg:"" help:"Name of the service and its executable."`
 	Path        string `arg:"" help:"Path to the service root and its working directory."`
 	Interactive bool   `short:"i" help:"Enable interactive configuration mode."`
 }
 
 func main() {
-	kong.Parse(&CLI)
+	var args Arguments
 
-	cfg := NewServiceConfig(CLI.Name, CLI.Path)
+	kong.Parse(&args)
 
-	if CLI.Interactive {
+	cfg := NewServiceConfig(args.Name, args.Path)
+
+	if args.Interactive {
+		var err error
+
+		cfg.NeedsNetwork, err = log.Confirm("NeedsNetwork", cfg.NeedsNetwork)
+		log.MustFail(err)
+
+		cfg.NeedsListening, err = log.Confirm("NeedsListening", cfg.NeedsListening)
+		log.MustFail(err)
+
+		cfg.NeedsExecMemory, err = log.Confirm("NeedsExecMemory", cfg.NeedsExecMemory)
+		log.MustFail(err)
+
+		cfg.NeedsWritableFiles, err = log.Confirm("NeedsWritableFiles", cfg.NeedsWritableFiles)
+		log.MustFail(err)
+
+		cfg.NeedsPublicTmp, err = log.Confirm("NeedsPublicTmp", cfg.NeedsPublicTmp)
+		log.MustFail(err)
+
+		cfg.NeedsDevices, err = log.Confirm("NeedsDevices", cfg.NeedsDevices)
+		log.MustFail(err)
+
+		cfg.NeedsSubprocess, err = log.Confirm("NeedsSubprocess", cfg.NeedsSubprocess)
+		log.MustFail(err)
 	}
 
 	log.Println("Writing configs...")
